8e0739ad0f0cd31c8caeac5c42e9812bd0ace140
Mikhail Novosyolov <mikhailnov@dumalogiya.ru>
2022-05-31 23:56:42 +0300

    Begin implementation

---
+++ freekazna.sh
@@ -0,0 +1,170 @@
+#!/usr/bin/env bash
+
+#set -x
+set -e
+set -f
+set -u
+set -o pipefail
+
+# echo error
+_ee(){
+	echo "$@" 1>&2
+}
+
+# https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
+XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+FREEKAZNA_DB_DIR="${FREEKAZNA_DB_DIR:-$XDG_DATA_HOME/freekazna}"
+FREEKAZNA_DB_TRANSACTIONS="${FREEKAZNA_DB_TRANSACTIONS:-$FREEKAZNA_DB_DIR/transactions.csv}"
+
+# https://github.com/greggles/mcc-codes
+FREEKAZNA_MCC_DB="${FREEKAZNA_MCC_DB:-}"
+if [ -z "$FREEKAZNA_MCC_DB" ]; then
+	if [ -f mcc_codes.csv ]; then
+		FREEKAZNA_MCC_DB="mcc_codes.csv"
+	elif [ -f "@DATADIR@/freekazna/mcc_codes.csv" ]; then
+		FREEKAZNA_MCC_DB="@DATADIR@/freekazna/mcc_codes.csv"
+	else
+		_ee "Database with MCC codes not found!"
+		exit 1
+	fi
+fi
+
+# Currently only avangard.ru is supported
+# Pull requests and patches with support of CSVs from other banks are welcomed!
+BANK="${BANK:-avangard}"
+SUPPORTED_BANKS=(avangard)
+
+CURRENCY_SIGN="${CURRENCY_SIGN:-₽}"
+		
+# $1: commit message
+_git(){
+	[ -n "${1:-}" ]
+	mkdir -p "$FREEKAZNA_DB_DIR"
+	pushd "$FREEKAZNA_DB_DIR" >/dev/null
+	if ! test -d ".git"; then
+		git init
+		git config user.name FreeKazna
+		git config user.email freekazna@localhost.tld
+	fi
+	git add .
+	git commit -m "$1"
+	if [ "$(git remote -v | grep -c .)" -gt 0 ]; then
+		git push
+	fi
+	popd
+}
+
+# $1: bank
+_check_bank_is_supported(){
+	local ok
+	ok=0
+	for (( a = 0; a < ${#SUPPORTED_BANKS[@]}; a++ ))
+	do
+		if [ "$1" = "${SUPPORTED_BANKS[$a]}" ]; then
+			ok=1
+			break
+		fi
+	done
+	test "$ok" = 1
+}
+
+# $1: MCC code
+_mcc2description(){
+	local o
+	IFS=',' read -a o <<< "$(set -e && set -o pipefail && grep "^$1," "$FREEKAZNA_MCC_DB" | head -n1)"
+	[ -n "${o[1]}" ] || return 1
+	# second column from csv
+	echo "${o[1]}"
+}
+
+# $1: string
+_string2md5(){
+	# XXX Why is "|| return 1" needed despite set -e?
+	[ -n "$*" ] || return 1
+	local o
+	read -a o <<< "$(echo "$@" | md5sum)"
+	[ -n "${o[0]}" ]
+	echo "${o[0]}"
+}
+
+# $1: MD5 of the transaction string
+_is_transaction_in_db(){
+	grep -q "^${1};" "$FREEKAZNA_DB_TRANSACTIONS"
+}
+
+# $1: MD5 of the string from the original imported data
+# $2: time of transaction (UNIX timestamp)
+# $3: sum spent (how much money was spent in your currency)
+# $4: MCC code of the transaction (use "0" if no code)
+_add_transaction_to_db(){
+	if [[ "$*" =~ .*';'.* ]]; then
+		_ee "Error adding transaction into db: symbol ; is used as a delimeter"
+		return 1
+	fi
+	echo "${1};${2};${3};${4}" >> "$FREEKAZNA_DB_TRANSACTIONS"
+}
+
+# $1: date
+# Example:
+# 26.02.2022 20:20
+_date2unix__avangard(){
+	local arr1
+	IFS=' ' read -a arr1 <<< "$(echo "$1")"
+	local d
+	IFS='.' read -a d <<< "$(echo "${arr1[0]}")"
+	local day
+	day="${d[0]}"
+	local month
+	month="${d[1]}"
+	local year
+	year="${d[2]}"
+	local t
+	IFS=':' read -a t <<< "$(echo "${arr1[1]}")"
+	local hour
+	hour="${t[0]}"
+	local minute="${t[1]}"
+	local second
+	second=00
+	# XXX В выгрузке из Авангарда в городах с немосковским временем время московское или местное?
+	# Предположим, что московское.
+	date +%s --date="TZ=\"Europe/Moscow\" ${year}-${month}-${day} ${hour}:${minute}:${second}"
+}
+
+# $1: sum in rub.kop
+# Example:
+# 560.50 (560 rub 50 kop)
+# results to 56050 kop.
+_sum2integer__avangard(){
+	local arr
+	IFS='.' read -a arr <<< "$(echo "$1 * 100" | bc)"
+	echo "${arr[0]}"
+}
+
+# Example CSV from Avangard is in test-data/avangard.csv
+# $1: line
+_line2db__avangard(){
+	local arr
+	# XXX For now iconv is actually not needed because we do not use that field
+	IFS=';' read -a arr <<< "$(echo "$1" | iconv -f cp1251 | sed -e 's,",,g')"
+	local md5
+	local time
+	local sum
+	local mcc
+	md5="$(_string2md5 "$1")"
+	time="$(_date2unix__avangard "${arr[4]}")"
+	sum="$(_sum2integer__avangard "${arr[2]}")"
+	mcc="${arr[8]}"
+	echo "${md5};${time};${sum};${mcc}"
+}
+
+_main(){
+	if ! _check_bank_is_supported "$BANK" ; then
+		_ee "Bank $BANK is not supported"
+		return 1
+	fi
+}
+
+if [ "${SOURCING:-0}" = 0 ]; then
+	_main "$@"
+fi
+	
---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+set -f
+set -u
+set -o pipefail
+
+SOURCING=1
+. ./freekazna.sh
+
+trap 'echo $?' EXIT
+
+echo "$CURRENCY_SIGN"
+test "$(_mcc2description 4816)" = "Computer Network Services"
+test "$(_string2md5 fff)" = "8b8a8b353298f798e3eb8628661617b6"
+_check_bank_is_supported avangard
+! _check_bank_is_supported novokukuevobank
+
+test "$(_date2unix__avangard '26.02.2022 20:20')" = 1645896000
+test "$(_sum2integer__avangard '123.45')" = 12345
+test "$(_line2db__avangard "$(head -n 1 test-data/avangard.csv)")" = "5d23fa9895e0ff5af1557d49d5cab1ca;1645896000;14500;5912"
